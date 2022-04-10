6aa925419432717ad1a801670353e97d84784138
unknown <markothedevmail@gmail.com>
2022-04-09 17:22:32 -0700

    small lil spelling correction

--- src/pages/Destination/Destination.tsx
+++ src/pages/Destination/Destination.tsx
@@ -10,14 +10,14 @@ import DestinationTab from "./DesitinationTab";
 const  Destination= ()=>{
     const Content = [
         {
-            "nav":"moon",
+            "nav":"Moon",
             "body":`Moon See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.`,
             "avg_distance":" 384,400.km",
             "travel_time":"3 days"
         },
 
         {
-            "nav":"mars",
+            "nav":"Mars",
             "body":`
             Mars Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!
             `,
@@ -27,7 +27,7 @@ const  Destination= ()=>{
 
         
         {
-            "nav":"europa",
+            "nav":"Europa",
             "body":`
             Europa The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin. 
             `,
